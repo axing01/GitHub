268c99a37eeb1f30362e1fa34c6b63b6963670de
axing01 <1767146802@qq.com>
2015-09-07 23:39:37 +0800

    333

---
+++ src/com/axing/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.axing.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("ni hao");
+		System.out.println("ni hao 2");
+		System.out.println("ni hao 3");
+		System.out.println("ni hao 4");
+		System.out.println("ni hao 5");
+		System.out.println("ni hao 6");
+	}
+}
